a84efb6cacf592d822242a3947474c0ff0bbc3f7
cktang88 <arctangent20@yahoo.com>
2019-12-05 00:25:44 -0600

    wk5 done

--- 5.py
+++ 5.py
@@ -3,7 +3,7 @@ arr = [l.strip() for l in inp]
 arr = [int(i) for i in arr[0].split(',')]
 # arr[1] = 82
 # arr[2] = 98
-INPUT = 1
+INPUT = 5
 print(len(arr), arr)
 a = 0
 
@@ -27,10 +27,11 @@ while a < len(arr):
         break
 
     i,j,k = f(arr[a+1], c), 0,0
-    if de < 3:
+    if de != 3 and de != 4:
         j = f(arr[a+2], b)
-        k = arr[a+3] # always writes to position mode
-    print(i,j,k)
+    if de < 3 or de > 6:
+        k = f(arr[a+3], a)
+    # print(i,j,k)
     
     if de == 1:
         arr[k] = i + j
@@ -44,9 +45,24 @@ while a < len(arr):
     elif de == 4:
         print('out: ', i)
         a += 2
+    elif de == 5:
+        if i != 0:
+            a = j
+        else:
+            a += 3
+    elif de == 6:
+        if i == 0:
+            a = j
+        else:
+            a += 3
+    elif de == 7:
+        arr[k] = (1 if i < j else 0)
+        a += 4
+    elif de == 8:
+        arr[k] = (1 if i == j else 0)
+        a += 4
     else:
         print('inv: ', de)
         break
-    print(arr[:30])
 # print(arr)
 print(arr[0])
