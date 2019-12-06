fca668c51d0d124b0dadfa69cc55b779de8576bf
cktang88 <arctangent20@yahoo.com>
2019-12-05 23:45:34 -0600

    wk6

---
+++ 6.py
@@ -0,0 +1,57 @@
+inp = open('6.txt','r')
+arr = [l.strip() for l in inp]
+d = {}
+for i in arr:
+    a,b = i.split(')')
+    if a not in d:
+        d[a] = []
+    d[a].append(b)
+# print(d)
+
+# bound = d['COM']
+# cnts = {}
+# for i in bound:
+#     cnts[i] = 1
+# while len(bound) > 0:
+#     i = bound.pop(0)
+#     if i in d:
+#         # print(i, d[i])
+#         bound.extend(d[i])
+#         for j in d[i]:
+#             cnts[j] = cnts[i] + 1
+# print(sum(cnts.values()), bound)
+
+newd = {}
+for k in d:
+    for i in d[k]:
+        if i not in newd:
+            newd[i] = set()
+        newd[i].add(k)
+for i in newd:
+    if i in d:
+        newd[i] = newd[i].union(d[i])
+
+# print(newd)
+
+bound = newd['YOU']
+cnts = {}
+for i in bound:
+    cnts[i] = 1
+seen = set()
+got = False
+while len(bound) > 0:
+    i = bound.pop()
+    # print(i)
+    if i in newd:
+        arr = newd[i].difference(seen)
+        if 'SAN' in arr:
+            print('boom', cnts[i])
+            got = True
+            break
+        bound = bound.union(arr)
+        for j in arr:
+            cnts[j] = cnts[i] + 1
+        # print(arr)
+    seen.add(i)
+print('Verified: ', got)
+
