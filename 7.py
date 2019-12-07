ccb76305310c99a4c32ef4df75dc64d482cf4f36
cktang88 <arctangent20@yahoo.com>
2019-12-07 00:40:36 -0600

    wk7 wip

--- 7.py
+++ 7.py
@@ -1,16 +1,16 @@
 import itertools
 
+inp = open('7small.txt','r')
+arr = [l.strip() for l in inp]
+arr = [int(i) for i in arr[0].split(',')]
+arrs = [arr for i in range(5)]
 
-def program(inputs):
-
-    inp = open('7.txt','r')
-    arr = [l.strip() for l in inp]
-    arr = [int(i) for i in arr[0].split(',')]
+def program(inputs, arr, startloc):
     # arr[1] = 82
     # arr[2] = 98
     # INPUT = 5
-    # print(len(arr), arr)
-    a = 0
+    print(len(arr), arr)
+    a = startloc
 
     def f(n, mode):
         if n > len(arr) - 1 or n < 0:
@@ -29,7 +29,7 @@ def program(inputs):
         # A = u%10
         # u //=10
         if de == 99:
-            return 'halt'
+            return 'halt',a,arr
             break
 
         i,j,k = f(arr[a+1], c), 0,0
@@ -50,8 +50,8 @@ def program(inputs):
             a += 2
         elif de == 4:
             print('out: ', i)
-            return i
             a += 2
+            return i,a,arr
         elif de == 5:
             if i != 0:
                 a = j
@@ -71,19 +71,29 @@ def program(inputs):
         else:
             print('inv: ', de)
             break
+    return 'halt',a,arr
     # print(arr)
     # print(arr[0])
 
 perms = itertools.permutations([5,6,7,8,9])
-
+locs = [0,0,0,0,0]
 def engine(perm):
     output = 0
+    lastout = 0
     q = 0
     while output != 'halt':
-        output = program([perm[q%5], output])
+        inarr = [perm[q%5], output] if locs[q%5] == 0 else [output]
+        print('start with', inarr, locs[q%5])
+        output, newloc, newarr = program(inarr, arrs[i], locs[q%5])
+        arrs[i] = newarr
+        if output != 'halt':
+            lastout = output
+        locs[q%5] = newloc
         q += 1
-    print(perm, output)
-    return output
+        print('endlocs',locs)
+        raw_input("cont...")
+    print(perm, lastout, locs)
+    return lastout
 
 ans = []
 for perm in perms:
